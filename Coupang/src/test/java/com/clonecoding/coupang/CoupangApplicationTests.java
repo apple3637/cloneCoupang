f447100280aac059d8ed2a72fae88da45209862b
stella lim <apple3637@gmail.com>
2019-09-22 13:44:39 +0900

    create

---
+++ Coupang/src/test/java/com/clonecoding/coupang/CoupangApplicationTests.java
@@ -0,0 +1,16 @@
+package com.clonecoding.coupang;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CoupangApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
